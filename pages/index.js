3f3117c62b2f8eaeaf46b20f8b6fbc128dd9043b
Michael Nunan <michael.nunan@outlook.com>
2023-07-11 20:12:39 -0500

    mvp

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,33 @@
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [btn, setBtn] = useState('Get a joke');
+  const [joke, setJoke] = useState('');
+  const [delivery, setDelivery] = useState('');
+
+  const sortJoke = (data) => {
+    const part1 = data.setup;
+    const part2 = data.delivery;
+    return [part1, part2];
+  };
+
+  const handleClick = () => {
+    if (btn === 'Get a joke' || btn === 'Get another joke') {
+      setBtn('Get the punchline');
+      document.getElementById('punch-line').hidden = true;
+      getJoke()
+        .then(sortJoke)
+        .then((arr) => {
+          setJoke(arr[0]);
+          setDelivery(arr[1]);
+        });
+    } else if (btn === 'Get the punchline') {
+      document.getElementById('punch-line').hidden = false;
+      setBtn('Get another joke');
+    }
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +38,9 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <button type="button">Get a joke</button>
+      <h1>{joke}</h1>
+      <h4 id="punch-line" hidden>{delivery}</h4>
+      <button type="button" onClick={handleClick}>{btn}</button>
     </div>
   );
 }
